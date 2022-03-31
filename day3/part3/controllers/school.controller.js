6fe3b12a6255bcce3a0a8ebb1700425275383dc8
Saruul <s.saruululzii@gmail.com>
2022-03-30 21:15:31 -0500

    Part2 Range fixes

--- day3/part3/controllers/school.controller.js
+++ day3/part3/controllers/school.controller.js
@@ -8,7 +8,7 @@ module.exports.getStudentById = function (req, res) {
   let rowIndex = req.params.studentRowIndex;
   if (rowIndex < 0 || studentsData.length < rowIndex) {
     res.status(404).json({
-      NotFound: `Please choose :studentRowIndex param in [0-${studentsData.length}]`,
+      NotFound: `Please choose :studentRowIndex param in [1-${studentsData.length}]`,
     });
   } else {
     res.status(200).json(studentsData[req.params.studentRowIndex - 1]);
