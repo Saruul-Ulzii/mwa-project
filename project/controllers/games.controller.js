cb22b68f0d2b26874bde0486fd49ff8d0f4d99d2
Saruul <s.saruululzii@gmail.com>
2022-03-31 23:43:26 -0500

    Day4 MongoDB Messages added into .env

--- project/controllers/games.controller.js
+++ project/controllers/games.controller.js
@@ -8,7 +8,7 @@ const getCollection = function (collectionName) {
 
 const getAll = function (req, res) {
   let offset = 0;
-  let count = 3;
+  let count = parseInt(process.env.GAMES_PAGINATION_LIMIT_DEFAULT_VALUE);
 
   if (req.query && req.query.offset) {
     offset = parseInt(req.query.offset);
@@ -17,7 +17,7 @@ const getAll = function (req, res) {
   if (req.query && req.query.count) {
     count = Math.min(
       parseInt(req.query.count),
-      process.env.GAMES_FETCH_MAX_COUNT
+      process.env.GAMES_PAGINATION_COUNT_MAX_VALUE
     );
   }
 
@@ -51,26 +51,23 @@ const getOne = function (req, res) {
 const addOne = function (req, res) {
   let newGame = {};
 
-  console.log(req.body);
   if (req.body) {
     let minPlayers = parseInt(req.body.minPlayers);
-    console.log(minPlayers);
     if (
       !minPlayers ||
       minPlayers < process.env.GAME_PLAYER_COUNT_MIN ||
       minPlayers > process.env.GAME_PLAYER_COUNT_MAX
     ) {
-      res.status(200).json({
+      res.status(400).json({
         Message: `${process.env.GAME_PLAYER_COUNT_MESSAGE} [${process.env.GAME_PLAYER_COUNT_MIN}-${process.env.GAME_PLAYER_COUNT_MAX}]`,
       });
       return;
     }
     newGame.minPlayers = minPlayers;
-  }
-  if (req.body && req.body.age) {
+
     let age = parseInt(req.body.age);
-    console.log(age);
     if (
+      !age ||
       age < process.env.GAME_PLAYER_AGE_MIN ||
       age > process.env.GAME_PLAYER_AGE_MAX
     ) {
@@ -80,36 +77,39 @@ const addOne = function (req, res) {
       return;
     }
     newGame.age = age;
-  }
-  if (req.body && req.body.price) {
+
     let price = parseFloat(req.body.price);
-    if (price < 0) {
+    if (!price || price < 0) {
       res.status(400).json({
         Message: process.env.GAME_PRICE_REQUIRED_MESSAGE,
       });
       return;
     }
     newGame.price = price;
-  }
-  if (req.body && req.body.title) {
-    let title = parseFloat(req.body.title);
-    if (title || title == "") {
+
+    let title = req.body.title;
+    if (!title) {
       res.status(400).json({
         Message: process.env.GAME_TITLE_REQUIRED_MESSAGE,
       });
       return;
     }
     newGame.title = title;
-  }
-  if (newGame != {}) {
-    const gamesCollection = getCollection(process.env.GAMES_COLLECTION_NAME);
-    gamesCollection.insertOne(newGame, function (err, savedGame) {
-      if (err) {
-        res.status(500).json({ error: err });
-      } else {
-        res.status(201).json(savedGame);
-      }
-    });
+
+    if (newGame != {}) {
+      const gamesCollection = getCollection(process.env.GAMES_COLLECTION_NAME);
+      gamesCollection.insertOne(newGame, function (err, savedGame) {
+        if (err) {
+          res.status(500).json({ error: err });
+        } else {
+          res.status(201).json(savedGame);
+        }
+      });
+    } else {
+      res.status(500).json({ Message: process.env.GAME_OBJECT_EMPTY_MESSAGE });
+    }
+  } else {
+    res.status(500).json({ Message: process.env.REQUEST_BODY_EMPTY_MESSAGE });
   }
 };
 
