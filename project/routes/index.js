157384c885afb71a331fe0eb563c1a2c453fefe1
Saruul <s.saruululzii@gmail.com>
2022-04-01 00:03:10 -0500

    Day4 Env file updated

--- project/routes/index.js
+++ project/routes/index.js
@@ -2,9 +2,14 @@ const express = require("express");
 const router = express.Router();
 const gameController = require("../controllers/games.controller");
 
-router.route("/games").get(gameController.getAll).post(gameController.addOne);
+router
+  .route(process.env.GAMES_URL)
+  .get(gameController.getAll)
+  .post(gameController.addOne);
 
-router.route("/games/:gameId").get(gameController.getOne);
-router.route("/games/:gameId").delete(gameController.deleteOne);
+router
+  .route(process.env.GAME_BY_ID_URL)
+  .get(gameController.getOne)
+  .delete(gameController.deleteOne);
 
 module.exports = router;
