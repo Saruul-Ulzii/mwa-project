1440af21fa6e13ae03580faccfb92ebb93a18338
Saruul <s.saruululzii@gmail.com>
2022-04-02 09:09:43 -0500

    Day5 select added

--- project/api/controllers/publishers.controller.js
+++ project/api/controllers/publishers.controller.js
@@ -3,15 +3,17 @@ const GameSchema = mongoose.model(process.env.GAME_MODEL_NAME);
 
 const getOne = function (req, res) {
   if (req.params.gameId) {
-    GameSchema.findById(req.params.gameId).exec(function (err, game) {
-      if (err) {
-        console.error(err);
-        res.status(400).json({ Message: process.env.CONTACT_ADMIN_MESSAGE });
-        return;
-      }
+    GameSchema.findById(req.params.gameId)
+      .select("publisher")
+      .exec(function (err, game) {
+        if (err) {
+          console.error(err);
+          res.status(400).json({ Message: process.env.CONTACT_ADMIN_MESSAGE });
+          return;
+        }
 
-      res.status(200).json(game.publisher);
-    });
+        res.status(200).json(game.publisher);
+      });
   } else {
     res.status(400).json({ Message: process.env.GAME_ID_REQUIRED_MESSAGE });
   }
